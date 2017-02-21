6087bb23a7a901aa1ecfefabd927af6e86a5346b
lab <lab@lab>
2017-02-21 17:58:26 +0900

    format source

--- tensorflow.org/03-mnist-for-ml-beginner.py
+++ tensorflow.org/03-mnist-for-ml-beginner.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
@@ -11,9 +10,6 @@ W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 
 y = tf.matmul(x, W) + b
-#   (?, 784) * (784 * 10) + (10, ?)
-# = (? * 10)              + (10, ?) # matmul
-# = (? * 10)              + (?, 10) # broadcasting
 
 cross_entropy = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
