bccf0ec5859bd22fbc123c53dbc14c0313df9689
lab <lab@lab>
2017-03-10 18:42:51 +0900

    imshow default  cmap : gray_r

--- tensorflow.org/mnist-data.py
+++ tensorflow.org/mnist-data.py
@@ -3,7 +3,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 import matplotlib.pyplot as plt
 
-mnist = input_data.read_data_sets("MNIST_data", one_hot=False)
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
 sess = tf.Session()
 
@@ -16,8 +16,8 @@ print(sess.run(sum_of_validation_labels))
 print(sess.run(sum_of_test_labels))
 
 images = np.reshape(mnist.train.images, [-1, 28, 28]);
-nega_image = np.reshape([1.] * 28 * 28, [28, 28])
-nega_images = nega_image - images
+# nega_image = np.reshape([1.] * 28 * 28, [28, 28])
+# nega_images = nega_image - images
 labels = mnist.train.labels
 
 row_size = 10
@@ -27,12 +27,11 @@ fig, axes = plt.subplots(row_size, col_size)
 
 for i in range(row_size):
     for j in range(col_size):
-        index = i * row_size + j
-        print index
+        index = i * col_size + j
         ax = axes[i, j]
-        ax.imshow(nega_images[index], cmap='gray')
+        ax.imshow(-images[index], cmap='gray')
         ax.axis('off')
-        ax.text(0, 0, labels[index])
+        # ax.text(0, 0, labels[index])
 
 plt.subplots_adjust(wspace=0, hspace=0, left=None, right=None, bottom=None, top=None)
 plt.show()
